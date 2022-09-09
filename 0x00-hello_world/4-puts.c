e6a5acfcbd8193d303ddc4655b7905c6bb644f6e
Kofoworawealth <mojeedkofoworola2020@gmail.com>
2022-09-09 02:13:23 -0700

    task in c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - output
+ * 
+ * Return always 0 (correct)
+ * /
+ int main(void)
+ {
+   puts("\"Programming is like building a multilingual puzzle")
+   return (0);
+   }
