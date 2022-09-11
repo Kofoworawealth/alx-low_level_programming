f6e7904570f572d18fcbeb3d95253f08e2608040
Kofoworawealth <mojeedkofoworola2020@gmail.com>
2022-09-11 03:57:31 -0700

    c variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,7 +11,7 @@ int main(void)
 	int n;
 	char last[] = "Last digit of";
 
-        srand(time(0));
+       	Srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("%s %d is %d and is ", last, n, n % 10);
 	if (n % 10 > 5)
@@ -27,7 +27,4 @@ int main(void)
 		printf("less than 6 and not 0\n");
 	}
 	return (0);
-}	
-
-
-
+}
