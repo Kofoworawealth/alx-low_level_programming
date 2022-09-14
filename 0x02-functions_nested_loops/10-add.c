69acfdfb3f968978362641757e388538654824e3
Kofoworawealth <mojeedkofoworola2020@gmail.com>
2022-09-13 18:21:52 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Return: the sum of the two operands
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
