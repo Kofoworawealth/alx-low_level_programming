d2e29be135560c00079d877937d237fe73d779e9
Kofoworawealth <mojeedkofoworola2020@gmail.com>
2022-09-16 21:57:23 -0700

    c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,15 +9,15 @@
  */
 
 int largest_number(int a, int b, int c)
-{int largest;
-	
+{
+	int largest;
+
 	if (a > b && a > c)
 	{
 		largest = a;
 	}
 	else if (b > c)
 	{
-		
 		largest = b;
 	}
 	else
