785b25173feb3dc1482c7764c92d87b4630108d5
Kofoworawealth <mojeedkofoworola2020@gmail.com>
2022-09-26 21:21:38 -0700

    c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,21 +9,19 @@
  */
 char *_strchr(char *s, char c)
 {
-  int a = 0, b;
-  
-  while (s[a])
-    {
-      a++;
-    }
-  
-  for (b = 0; b <= a; b++)
-    {
-      if (c == s[b])
-	{
-	  s += b;
-	  return (s);
-	}
-    }
-  
-  return ('\0');
+int a = 0, b;
+
+while (s[a])
+{
+a++;
+}
+for (b = 0; b <= a; b++)
+{
+if (c == s[b])
+{
+s += b;
+return (s);
+}
+}
+return ('\0');
 }
