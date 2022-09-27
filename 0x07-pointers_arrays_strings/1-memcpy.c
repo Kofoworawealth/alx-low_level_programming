d70502e3918a6ad9a4e787397dea5e72b5710c0b
Kofoworawealth <mojeedkofoworola2020@gmail.com>
2022-09-26 21:15:01 -0700

    c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,10 +9,9 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-  unsigned int a;
-  
-  for (a = 0; a < n; a++)
-    dest[a] = src[a];
-  
-  return (dest);
+unsigned int a;
+
+for (a = 0; a < n; a++)
+dest[a] = src[a];
+return (dest);
 }
