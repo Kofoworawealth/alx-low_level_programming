7f8289a7a134bb6829f8b8d0ba554e60c8ab8eb9
Kofoworawealth <mojeedkofoworola2020@gmail.com>
2022-09-30 08:52:50 -0700

    c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints its name
+ * @argc: argc param
+ * @argv: an array of a command listed
+ * Return: 0 for success
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
